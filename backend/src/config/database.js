072ac14ca3ca30ba4835866cab834fecc5b0539f
Sudipto Ghosh <sudipto@ghosh.pro>
2020-10-04 23:19:07 +0530

    ci appeasement trial 1

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,7 +3,7 @@ const Sequelize = require("sequelize");
 module.exports = new Sequelize.Sequelize(
   process.env.DB_URL
     ? process.env.DB_URL
-    : "postgres://stayio:stayio@delhi.ghosh.pro:5999/test",
+    : "postgres://stayio:stayio@delhi.ghosh.pro:5999/stayio",
   {
     logging: process.env.NODE_ENV === "production" ? true : false,
   }
